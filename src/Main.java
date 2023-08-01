805fa7d167497a8c85121f8159bd0541a3492f8c
nyraa <112930946+nyraa@users.noreply.github.com>
2023-08-02 05:02:33 +0800

    check for args.length

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        if(args.length < 1)
+        {
+            JOptionPane.showMessageDialog(null, "Base dir missing", "Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
         Window window = new Window(args[0]);
         window.setVisible(true);
     }
